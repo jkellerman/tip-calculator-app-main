38eb0b683d676cad0393cad2c65d0842b0adf5d1
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-02-12 16:36:08 +0000

    add span to values so that we can access these values as integers in JS

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const a = "15%";
+const billInput = document.querySelector("#bill");
+const numOfPeopleInput = document.querySelector("#number-of-people");
+const customTipInput = document.querySelector("#custom-tip");
+const buttons = document.querySelectorAll(".button");
+let tipAmountValue = document.querySelector(".tip-amount-value").innerText;
 
-console.log(parseInt(a));
+// console.log(customTipInput);
+// console.log(billInput);
+// console.log(numOfPeopleInput);
+// console.log(buttons);
+console.log(tipAmountValue);
