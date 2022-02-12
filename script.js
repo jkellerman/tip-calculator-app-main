c5662f8f7bc8581e8d81a5b542cbcd1e45141363
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-02-12 17:34:02 +0000

    add functionality for reset button

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+const inputs = document.querySelectorAll("input");
 const billInput = document.querySelector("#bill");
 const numOfPeopleInput = document.querySelector("#number-of-people");
 const customTipInput = document.querySelector("#custom-tip");
 const buttons = document.querySelectorAll(".button");
-let tipAmountValue = document.querySelector(".tip-amount-value").innerText;
+let tipAmountValue = document.querySelector(".tip-amount-value");
+let totalValue = document.querySelector(".total-value");
+const resetButton = document.querySelector(".reset");
+let buttonClicked = false; // incase we need a variable that states button has been clicked
+let clickedButtonValue = 0; // incase we need a variable that gives us the value of the clicked button
 
-// console.log(customTipInput);
-// console.log(billInput);
-// console.log(numOfPeopleInput);
-// console.log(buttons);
-console.log(tipAmountValue);
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let inputValue = parseInt(e.data);
+    if (typeof inputValue === "number") {
+      resetButton.style.opacity = "1";
+      resetButton.style.cursor = "pointer";
+
+      resetButton.addEventListener("click", () => {
+        inputs.forEach((input) => {
+          input.value = "";
+        });
+      });
+    }
+  });
+});
+
+// let a = 10;
+// tipAmountValue.innerText = a.toFixed(2);
