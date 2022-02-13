31f50adaa0141d31ac684952fb39aaf189f19223
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-02-13 17:33:57 +0000

    add active states for buttons and add functionality for when one button is clicked other buttons remove active class

--- script.js
+++ script.js
@@ -3,13 +3,48 @@ const resetButton = document.querySelector(".reset");
 const billInput = document.querySelector("#bill");
 const customTipInput = document.querySelector("#custom-tip");
 const numOfPeopleInput = document.querySelector("#number-of-people");
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("mousedown", () => {
+    let buttonValue = parseInt(button.innerText);
+    customTipInput.value = "";
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+    });
+    button.classList.add("active");
+    calculate(buttonValue);
+  });
+});
 
 // Get bill input when input is selected & calculate
-billInput.addEventListener("input", calculate);
-customTipInput.addEventListener("input", calculate);
-numOfPeopleInput.addEventListener("input", calculate);
+billInput.addEventListener("input", customCalculate);
+customTipInput.addEventListener("input", customCalculate);
+numOfPeopleInput.addEventListener("input", customCalculate);
 
-function calculate() {
+function calculate(tipPercent) {
+  if (billInput.value >= 1 && numOfPeopleInput.value >= 1) {
+    let billInputValue = parseFloat(document.querySelector("#bill").value);
+    let numOfPeopleValue = parseInt(
+      document.querySelector("#number-of-people").value
+    );
+    let totalMinusTip = billInputValue / numOfPeopleValue;
+    let tipPerPerson = totalMinusTip * (tipPercent / 100);
+    let totalPerPerson = totalMinusTip + tipPerPerson;
+
+    let tipValue = document.querySelector(".tip-amount-value");
+    tipValue.innerText = tipPerPerson.toFixed(2);
+    let totalValue = document.querySelector(".total-value");
+    totalValue.innerText = totalPerPerson.toFixed(2);
+  } else {
+    let tipValue = document.querySelector(".tip-amount-value");
+    tipValue.innerText = parseFloat("0").toFixed(2);
+    let totalValue = document.querySelector(".total-value");
+    totalValue.innerText = parseFloat("0").toFixed(2);
+  }
+}
+
+function customCalculate() {
   if (
     billInput.value >= 1 &&
     customTipInput.value >= 1 &&
