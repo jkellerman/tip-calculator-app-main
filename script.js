d45d25b6bb1618f4ea7b8c542e5228a206fe283c
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-02-14 11:11:14 +0000

    add functionality for calculation when a select tip button is clicked

--- script.js
+++ script.js
@@ -5,6 +5,55 @@ const customTipInput = document.querySelector("#custom-tip");
 const numOfPeopleInput = document.querySelector("#number-of-people");
 const buttons = document.querySelectorAll(".button");
 
+// activate reset button when number is inputted into any input field and clear input fields when reset button is clicked
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let inputNumber = parseFloat(e.data);
+    if (typeof inputNumber === "number") {
+      resetButton.style.opacity = "1";
+      resetButton.style.cursor = "pointer";
+
+      resetButton.addEventListener("mouseenter", () => {
+        resetButton.style.backgroundColor = "var(--hover)";
+        resetButton.style.borderColor = "var(--hover)";
+        resetButton.style.transition = "0.2s ease-in-out";
+      });
+
+      resetButton.addEventListener("mouseleave", () => {
+        resetButton.style.backgroundColor = null;
+        resetButton.style.borderColor = null;
+      });
+
+      // clear input fields and  tip totals
+
+      resetButton.addEventListener("click", () => {
+        inputs.forEach((input) => {
+          input.value = "";
+        });
+        let tipValue = document.querySelector(".tip-amount-value");
+        tipValue.innerText = parseFloat("0").toFixed(2);
+        let totalValue = document.querySelector(".total-value");
+        totalValue.innerText = parseFloat("0").toFixed(2);
+      });
+    }
+  });
+});
+
+// add event listeners for input fields which perform calculation when custom tip is selected
+
+billInput.addEventListener("input", customCalculate);
+numOfPeopleInput.addEventListener("input", customCalculate);
+customTipInput.addEventListener("input", () => {
+  if (customTipInput.value >= 1) {
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+    });
+  }
+  customCalculate();
+});
+
+// add event listeners for buttons which perform calculation when they are selected
+
 buttons.forEach((button) => {
   button.addEventListener("mousedown", () => {
     let buttonValue = parseInt(button.innerText);
@@ -17,25 +66,26 @@ buttons.forEach((button) => {
   });
 });
 
-billInput.addEventListener("input", customCalculate);
-customTipInput.addEventListener("input", () => {
-  if (customTipInput.value >= 1) {
-    buttons.forEach((button) => {
-      button.classList.remove("active");
-    });
-  }
-  customCalculate();
-});
-numOfPeopleInput.addEventListener("input", customCalculate);
+// add event listeners for inputs which perform calculations when buttons are selected
 
-function calculate(tipPercent) {
-  if (billInput.value >= 1 && numOfPeopleInput.value >= 1) {
-    let billInputValue = parseFloat(document.querySelector("#bill").value);
-    let numOfPeopleValue = parseInt(
-      document.querySelector("#number-of-people").value
-    );
+billInput.addEventListener("input", buttonCalculate);
+numOfPeopleInput.addEventListener("input", buttonCalculate);
+
+function buttonCalculate() {
+  let activeButton = document.querySelector(".active");
+  let billInputValue = parseFloat(document.querySelector("#bill").value);
+  let numOfPeopleValue = parseInt(
+    document.querySelector("#number-of-people").value
+  );
+
+  if (
+    billInput.value >= 1 &&
+    numOfPeopleInput.value >= 1 &&
+    activeButton.classList.contains("active")
+  ) {
+    let activeButtonValue = parseInt(activeButton.innerText);
     let totalMinusTip = billInputValue / numOfPeopleValue;
-    let tipPerPerson = totalMinusTip * (tipPercent / 100);
+    let tipPerPerson = totalMinusTip * (activeButtonValue / 100);
     let totalPerPerson = totalMinusTip + tipPerPerson;
 
     let tipValue = document.querySelector(".tip-amount-value");
@@ -79,35 +129,24 @@ function customCalculate() {
   }
 }
 
-// activate reset button when number is inputted into any input field and clear input fields when reset button is clicked
+function calculate(tipPercent) {
+  if (billInput.value >= 1 && numOfPeopleInput.value >= 1) {
+    let billInputValue = parseFloat(document.querySelector("#bill").value);
+    let numOfPeopleValue = parseInt(
+      document.querySelector("#number-of-people").value
+    );
+    let totalMinusTip = billInputValue / numOfPeopleValue;
+    let tipPerPerson = totalMinusTip * (tipPercent / 100);
+    let totalPerPerson = totalMinusTip + tipPerPerson;
 
-inputs.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    let inputNumber = parseFloat(e.data);
-    if (typeof inputNumber === "number") {
-      resetButton.style.opacity = "1";
-      resetButton.style.cursor = "pointer";
-
-      resetButton.addEventListener("mouseenter", () => {
-        resetButton.style.backgroundColor = "var(--hover)";
-        resetButton.style.borderColor = "var(--hover)";
-        resetButton.style.transition = "0.2s ease-in-out";
-      });
-
-      resetButton.addEventListener("mouseleave", () => {
-        resetButton.style.backgroundColor = null;
-        resetButton.style.borderColor = null;
-      });
-
-      resetButton.addEventListener("click", () => {
-        inputs.forEach((input) => {
-          input.value = "";
-        });
-        let tipValue = document.querySelector(".tip-amount-value");
-        tipValue.innerText = parseFloat("0").toFixed(2);
-        let totalValue = document.querySelector(".total-value");
-        totalValue.innerText = parseFloat("0").toFixed(2);
-      });
-    }
-  });
-});
+    let tipValue = document.querySelector(".tip-amount-value");
+    tipValue.innerText = tipPerPerson.toFixed(2);
+    let totalValue = document.querySelector(".total-value");
+    totalValue.innerText = totalPerPerson.toFixed(2);
+  } else {
+    let tipValue = document.querySelector(".tip-amount-value");
+    tipValue.innerText = parseFloat("0").toFixed(2);
+    let totalValue = document.querySelector(".total-value");
+    totalValue.innerText = parseFloat("0").toFixed(2);
+  }
+}
